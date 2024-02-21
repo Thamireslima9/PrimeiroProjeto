529cf079e44d26e0062110ba391de760c4ff1361
Thamireslima9 <thamires.silva@aluno.ifsp.edu.br>
2024-02-21 11:13:00 -0300

    calculadora

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -17,6 +17,31 @@
 
             Console.WriteLine(  );
             Console.WriteLine("nome: {0} \nidade: {1} \ncidade: {2}",nome, idade, cidade);
+
+            //Contas
+            Console.WriteLine( );
+            int num1, num2, soma, sub, mult, div;
+
+            Console.WriteLine("Digite o número a: ");
+                num1= int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o número b: ");
+                num2 = int.Parse(Console.ReadLine());
+
+            soma = num1 + num2;
+            sub = num1 - num2;
+            div = num1 / num2;
+            mult = num1 * num2;
+
+            Console.WriteLine("soma: {0} \nsubtração: {1} \ndivião: {2} \nmultiplicação: {3}", soma, sub, div,mult);
+
+
+
+
+
+
+
+
+
         }
     }
 }
